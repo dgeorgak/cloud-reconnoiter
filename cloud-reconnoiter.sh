78ccbece25d68e080f7ed7e953a3ee29fd172f20
Dim <dimgeorgakopoulos@gmail.com>
2025-03-30 22:41:29 +0300

    wget sni-ip files

---
+++ cloud-reconnoiter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Download latest sni-ip ranges from kaeferjaeger
+#wget https://kaeferjaeger.gay/sni-ip-ranges/amazon/ipv4_merged_sni.txt -O amazon-ipv4-sni-ip.txt
+wget https://kaeferjaeger.gay/sni-ip-ranges/digitalocean/ipv4_merged_sni.txt -O DO-ipv4-sni-ip.txt
+#wget https://kaeferjaeger.gay/sni-ip-ranges/google/ipv4_merged_sni.txt -O google-ipv4-sni-ip.txt
+#wget https://kaeferjaeger.gay/sni-ip-ranges/microsoft/ipv4_merged_sni.txt -O microsoft-ipv4-sni-ip.txt
+#wget https://kaeferjaeger.gay/sni-ip-ranges/oracle/ipv4_merged_sni.txt -O oracle-ipv4-sni-ip.txt
+
